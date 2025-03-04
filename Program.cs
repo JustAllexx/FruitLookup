333c96304820915d68efa27691d008f18663804d
Alex Wilkinson <alex.wilkinson@student.manchester.ac.uk>
2025-03-04 22:41:46 +0000

    Added https connection

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+class FruityLookup {
+    HttpClient client = new();
+
+    public FruityLookup() {
+        initialiseClient();
+    }
+
+    private void initialiseClient() {
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json")
+        );
+    }
+
+    public async Task MakeRequestAsync(String url) {
+        String json = await client.GetStringAsync(url);
+        Console.Write(json);
+    }
+}
+
+class FruityLookupCLI {
+
+    public static async Task<int> Main(String[] args) {
+        FruityLookup fruityLookup = new();
+        String url = "https://fruityvice.com/api/fruit/apple";
+        await fruityLookup.MakeRequestAsync(url);
+
+        return 0;
+    }
+}
