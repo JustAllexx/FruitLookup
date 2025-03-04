58f4de04589a51b2ed4591872775f3d618e5b5a7
Alex Wilkinson <alex.wilkinson@student.manchester.ac.uk>
2025-03-04 23:07:36 +0000

    Added Json Deserialisation

--- Program.cs
+++ Program.cs
@@ -1,6 +1,24 @@
 ﻿using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
+using System.Text.Json;
 using System.Threading.Tasks;
+
+public record class Nutrition {
+    public int? calories { get; init; }
+    public double fat { get; init; }
+    public double sugar { get; init; }
+    public double carbohydrates {get; init; }
+    public double protein { get; init; }
+}
+
+public record class Fruit {
+    public String? name { get; init; }
+    public int? id { get; init; }
+    public String? family { get; init; }
+    public String? order { get; init; }
+    public String? genus { get; init; }
+    public Nutrition? nutritions { get; init; }
+}
 
 class FruityLookup {
     HttpClient client = new();
@@ -17,8 +35,14 @@ class FruityLookup {
     }
 
     public async Task MakeRequestAsync(String url) {
-        String json = await client.GetStringAsync(url);
-        Console.Write(json);
+        Stream json = await client.GetStreamAsync(url);
+        Fruit? fruit = await JsonSerializer.DeserializeAsync<Fruit>(json);
+        if(fruit == null) {
+            Console.WriteLine("Deserialising went wrong");
+            return;
+        }
+
+        Console.Write(fruit);
     }
 }
 
