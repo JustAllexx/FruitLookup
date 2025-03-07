771dc78be93876e74184ea6f0426f6c039764d90
Alex Wilkinson <alex.wilkinson@student.manchester.ac.uk>
2025-03-07 18:08:11 +0000

    Did more work to Implement MAUI

--- FruityLookup.GUI/MainPage.xaml.cs
+++ FruityLookup.GUI/MainPage.xaml.cs
@@ -1,20 +1,27 @@
-﻿namespace FruityLookup.GUI {
+﻿using FruityLookup.Entities;
+using System.Threading.Tasks;
+
+namespace FruityLookup.GUI {
     public partial class MainPage : ContentPage {
         int count = 0;
+        FruityLookup fruityLookup = new();
 
         public MainPage() {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e) {
+        private async void OnCounterClicked(object sender, EventArgs e) {
             count++;
-
+            Fruit? fruit = await fruityLookup.getFruitInformationAsync("apple");
+            if (fruit != null) {
+                CounterBtn.Text = fruit.genus;
+            }
+            /*
             if (count == 1)
                 CounterBtn.Text = $"Clicked {count} time";
             else
                 CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            */
         }
     }
 
